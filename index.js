9a6b05f639d11136f57758f3364b73ef03972586
Roksana Niechcial <roksana.niechcial@u2i.com>
2020-09-06 23:35:15 +0200

    Prometheus

--- index.js
+++ index.js
@@ -2,17 +2,39 @@ const express = require("express") // our express server
 const cors = require('cors');
 const bodyParser = require("body-parser") // requiring the body-parser
 const db = require("./models/")
+const Prometheus = require('prom-client');
+
+const { collectDefaultMetrics, Registry } = Prometheus;
+
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['route'],
+  // buckets for response time from 0.1ms to 500ms
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500],
+});
 
 const app = express() // generate an app object
-const PORT = process.env.PORT || 9000 // port that the server is running on => localhost:3000
-
 app.use(cors());
-app.use(bodyParser.json()) // telling the app that we are going to use json to handle incoming payload
 
 function success(res, payload) {
   return res.status(200).json(payload)
 }
+const PORT = process.env.PORT || 9000 // port that the server is running on => localhost:3000
 
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now();
+  next();
+});
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+});
+
+
+app.use(bodyParser.json()) // telling the app that we are going to use json to handle incoming payload
 app.get("/users", async (req, res, next) => {
   try {
     const users = await db.User.find({})
@@ -57,9 +79,32 @@ app.use((err, req, res, next) => {
   })
 })
 
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now(); - res.locals.startEpoch;
+
+  httpRequestDurationMicroseconds
+      .labels(req.method, req.originalUrl, res.statusCode)
+      .observe(responseTimeInMs);
+
+  next();
+});
+
 app.listen(PORT, () => {
   // listening on port 3000
   console.log(`listening on port ${PORT}`) // print this when the server starts
+})
+
+process.on('SIGTERM', () => {
+  clearInterval(metricsInterval)
+
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    process.exit(0)
+  })
 })
 
 
