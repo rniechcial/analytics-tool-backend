b670f0eb91a624897e3097a3b96faa033ce7af31
Roksana Niechcial <roksana.niechcial@u2i.com>
2021-08-22 20:04:08 +0200

    Basic measurements

--- index.js
+++ index.js
@@ -2,12 +2,77 @@ const express = require("express") // our express server
 const cors = require('cors');
 const bodyParser = require("body-parser") // requiring the body-parser
 const db = require("./models/")
+const lynx = require("lynx");
+const request = require("request");
 
 const app = express() // generate an app object
 const PORT = process.env.PORT || 9000 // port that the server is running on => localhost:3000
+var opt = {};
+opt.prefix = 'UsersSampleApp';
+var metrics = new lynx('localhost', 8125, opt); // StatsD IP & Port
 
 app.use(cors());
 app.use(bodyParser.json()) // telling the app that we are going to use json to handle incoming payload
+
+
+//Example1 - Hit Count Home Page
+// app.get("/", (req, res) => {
+//   res.send("Welcome to HomePage");
+//   metrics.increment('HomePage.hitcount');
+// });
+
+// Example2 - Gauges App
+app.get("/GaugesApp", (req, res) => {
+  res.send("Welcome to GaugesApp Page");
+  request.get({
+    url : 'http://localhost:9000',
+    time : true
+  },function(err, response){
+    console.log('MWI Request time in ms', response.elapsedTime);
+    metrics.gauge('GaugesApp.usersApp', response.elapsedTime);
+  });
+  // request.get({
+  //   url : 'https://www.google.com',
+  //   time : true
+  // },function(err, response1){
+  //   console.log('Google Request time in ms', response1.elapsedTime);
+  //   metrics.gauge('GaugesApp.google', response1.elapsedTime);
+  // });
+});
+
+
+// //Example3 - Timer App
+// app.get("/TimerApp", (req, res) => {
+//   res.send("Welcome to TimerApp Page");
+//   request.get({
+//     url : 'https://www.middlewareinventory.com',
+//     time : true
+//   },function(err, response){
+//     console.log('Request time in ms', response.elapsedTime);
+//     metrics.timing('TimerApp.mwi', response.elapsedTime);
+//   });
+//
+//   request.get({
+//     url : 'https://www.google.com',
+//     time : true
+//   },function(err, response1){
+//     console.log('Request time in ms', response1.elapsedTime);
+//     metrics.timing('TimerApp.google', response1.elapsedTime);
+//   });
+// });
+
+
+// //Example4 - User Defined Sets
+// app.get("/Sets", (req, res) => {
+//   metrics.set('Sets.user', 'Sarav');
+//   metrics.set('Sets.user', 'Sarav');
+//   if (Math.random() > 0.9)
+//   {
+//     metrics.set('Sets.user', 'Jarvis');
+//   }
+//   setTimeout(pick, 1000);
+// });
+
 
 function success(res, payload) {
   return res.status(200).json(payload)
@@ -16,6 +81,7 @@ function success(res, payload) {
 app.get("/users", async (req, res, next) => {
   try {
     const users = await db.User.find({})
+    metrics.increment('Users.hitcount');
     return success(res, users)
   } catch (err) {
     next({ status: 400, message: "failed to get users" })
